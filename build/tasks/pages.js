67effebe8d756d0721f352c465e772b68cd72de8
Ron Buckton <rbuckton@chronicles.org>
2015-10-05 13:29:58 -0700

    Fixed issue with gh-pages publishing.

--- build/tasks/pages.js
+++ build/tasks/pages.js
@@ -9,15 +9,15 @@ module.exports = function pages() {
         if (!ecmarkup) {
             throw new Error("Please set the 'ECMARKUP'' environment variable.");
         }
-        
+
         return new Promise(function (resolve) { resolve(); })
             .then(function() { return git(["-C", "./pages", "pull", "--no-stat"]); })
             .then(function() { return gmd("./spec/es6.grammar", "./spec/es6.grammar.html", "ecmarkup"); })
             .then(function() { return gmd("./spec/typescript.grammar", "./spec/typescript.grammar.html", "ecmarkup"); })
             .then(function() { return emu("./spec/es6.html", "./pages/es6.html", { js: "./pages/menu.js", css: "./pages/elements.css" }); })
             .then(function() { return emu("./spec/typescript.html", "./pages/typescript.html", { }); })
-            .then(function() { return git(["add", "."], { cwd: "./pages/" }); })
-            .then(function() { return git(["commit", "-m", "Deploy to GitHub Pages"], { cwd: "./pages/" }); })
-            .then(function() { return git(["push", "--force", "--quiet"], { cwd: "./pages/" }); });
+            .then(function() { return git(["-C", "./pages", "add", "."]); })
+            .then(function() { return git(["-C", "./pages", "commit", "-m", "Deploy to GitHub Pages"]); })
+            .then(function() { return git(["-C", "./pages", "push", "--force", "--quiet"]); });
     };
 };
